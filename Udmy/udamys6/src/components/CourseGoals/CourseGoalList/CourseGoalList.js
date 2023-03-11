a2609d3f4cee35e63602ef22435248f04e8f5028
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-03-11 13:55:40 +0530

    Add files via upload

--- Udmy/udamys6/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ Udmy/udamys6/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -7,13 +7,7 @@ const CourseGoalList = props => {
   return (
     <ul className="goal-list">
       {props.items.map(goal => (
-        <CourseGoalItem
-          key={goal.id}
-          id={goal.id}
-          onDelete={props.onDeleteItem}
-        >
-          {goal.text}
-        </CourseGoalItem>
+        <CourseGoalItem key={goal.id} id={goal.id} onDelete={props.onDeleteItem}>{goal.text}</CourseGoalItem>
       ))}
     </ul>
   );
