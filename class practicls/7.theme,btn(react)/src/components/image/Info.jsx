8ff363aa37627fcb9bdac024638be193a57e6761
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-03-22 21:50:47 +0530

    Add files via upload

---
+++ class practicls/7.theme,btn(react)/src/components/image/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Info(props) {
+    return (
+        <li className='list-group-item'>
+            {props.label}
+        </li>
+    )
+}
+
+export default Info
