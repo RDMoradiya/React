bac04313979711c6d28ed4322f8e533baaf21463
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-03-25 20:01:59 +0530

    Add files via upload

---
+++ class practicls/9.form/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import './App.css'
+import Form from './components/Form'
+import FormList from './components/FormList'
+
+
+const App = () => {
+
+  const [userData, setUserData] = useState([])
+
+  const FormDataHandler = (username, email, password) => {
+    setUserData((prev) => {
+      return (
+        [...prev, { username: username, email: email, password: password }]
+      )
+    })
+  }
+
+  return (
+    <>
+      <Form formData={FormDataHandler} />
+      <FormList user={userData} />
+    </>
+  )
+}
+
+export default App;
