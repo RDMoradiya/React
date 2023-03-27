397eaeafc2b2a11024fc7ada3f4dc75694cb0215
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-03-28 00:16:59 +0530

    Add files via upload

---
+++ class practicls/12.cocktailApi_search/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import './NavBar.css'
+import { Cocktailwrapper } from '../App'
+
+function NavBar() {
+
+    const { drinkName, setDrinkName } = useContext(Cocktailwrapper)
+
+    return (
+        <>
+            <div className="container p-3 d-flex justify-content-between align-items-center">
+                <div>
+                    <h1><a href="/">Fruit  <span>Juicy</span></a></h1>
+                </div>
+                <div>
+                    <input placeholder="Type to search..." className="input" value={drinkName} onChange={(e) => setDrinkName(e.target.value)} />
+                    <div className="icon">
+                        <title>Search</title>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
