454075909546a9772835c2e1368cbf568937bb4d
RD Moradiya <rdmoradiya1217@gmail.com>
2023-04-03 19:16:35 +0530

    usenavigar_and_uselocation

---
+++ src/Pages/Pagenotfind.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Pagenotfind() {
+  return (
+    <div className="container py-3">
+      <h2>Page not found ..........</h2>
+      <hr />
+    </div>
+  );
+}
+
+export default Pagenotfind;
