31a415a193a29d1d1d03413bfaa421df7f9870a5
RD Moradiya <rdmoradiya1217@gmail.com>
2023-04-04 14:15:06 +0530

    router

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { ProductList } from "../components/ProductList";
+import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
+
+function Product() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="container py-3">
+      <h2>Products Page</h2>
+      <hr />
+      <div className="row d-flex justify-content-between">
+        {ProductList.map((item) => {
+          const { id, name, img } = item;
+          return (
+            <div
+              className="card mb-md-2 col-lg-3 col-xl-2 text-center "
+              key={id}
+            >
+              <div className="card-body">
+                <img src={img} alt="" className="w-100" />
+                <h5>{name}</h5>
+              </div>
+              <div className="card-footer text-center p-3">
+                <button className="btn btn-primary">
+                  <Link
+                    to={`/product/${id}`}
+                    className="text-decoration-none text-white"
+                  >
+                    more details ...
+                  </Link>
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Product;
