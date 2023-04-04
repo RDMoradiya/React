566dd115bce6f66d3e40e16386c8686f32b105b7
RD Moradiya <rdmoradiya1217@gmail.com>
2023-04-04 14:10:33 +0530

    practice

--- src/components/image/MovieData.jsx
+++ src/components/image/MovieData.jsx
@@ -1,39 +1,43 @@
 export const MoviesData = [
-    {
-        MovieName: 'Dangal',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg',
-        MovieDetails: "Nitesh Tiwari",
-        link: 'https://en.wikipedia.org/wiki/Dangal_(film)'
-    },
-    {
-        MovieName: 'Baahubali 2',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/9/93/Baahubali_2_The_Conclusion_poster.jpg',
-        MovieDetails: "S. S. Rajamouli",
-        link: 'https://en.wikipedia.org/wiki/Baahubali_2:_The_Conclusion'
-    },
-    {
-        MovieName: 'RRR',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/d/d7/RRR_Poster.jpg',
-        MovieDetails: "S. S. Rajamouli",
-        link: 'https://en.wikipedia.org/wiki/RRR_(film)'
-    },
-    {
-        MovieName: 'K.G.F: Chapter 2',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/d/d0/K.G.F_Chapter_2.jpg',
-        MovieDetails: "Prashanth Neel",
-        link: 'https://en.wikipedia.org/wiki/K.G.F:_Chapter_2'
-    },
-    {
-        MovieName: 'Pathaan',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/c/c3/Pathaan_film_poster.jpg',
-        MovieDetails: "Siddharth Anand",
-        link: 'https://en.wikipedia.org/wiki/Pathaan_(film)'
-
-    },
-    {
-        MovieName: 'Bajrangi Bhaijaan',
-        MovieImage: 'https://upload.wikimedia.org/wikipedia/en/d/dd/Bajrangi_Bhaijaan_Poster.jpg',
-        MovieDetails: "Kabir Khan",
-        link: 'https://en.wikipedia.org/wiki/Bajrangi_Bhaijaan'
-    }
-]
+  {
+    MovieName: "Dangal",
+    MovieImage:
+      "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg",
+    MovieDetails: "Nitesh Tiwari",
+    link: "https://en.wikipedia.org/wiki/Dangal_(film)",
+  },
+  {
+    MovieName: "Baahubali 2",
+    MovieImage:
+      "https://upload.wikimedia.org/wikipedia/en/9/93/Baahubali_2_The_Conclusion_poster.jpg",
+    MovieDetails: "S. S. Rajamouli",
+    link: "https://en.wikipedia.org/wiki/Baahubali_2:_The_Conclusion",
+  },
+  {
+    MovieName: "RRR",
+    MovieImage: "https://upload.wikimedia.org/wikipedia/en/d/d7/RRR_Poster.jpg",
+    MovieDetails: "S. S. Rajamouli",
+    link: "https://en.wikipedia.org/wiki/RRR_(film)",
+  },
+  {
+    MovieName: "K.G.F: Chapter 2",
+    MovieImage:
+      "https://upload.wikimedia.org/wikipedia/en/d/d0/K.G.F_Chapter_2.jpg",
+    MovieDetails: "Prashanth Neel",
+    link: "https://en.wikipedia.org/wiki/K.G.F:_Chapter_2",
+  },
+  {
+    MovieName: "Pathaan",
+    MovieImage:
+      "https://upload.wikimedia.org/wikipedia/en/c/c3/Pathaan_film_poster.jpg",
+    MovieDetails: "Siddharth Anand",
+    link: "https://en.wikipedia.org/wiki/Pathaan_(film)",
+  },
+  {
+    MovieName: "Bajrangi Bhaijaan",
+    MovieImage:
+      "https://upload.wikimedia.org/wikipedia/en/d/dd/Bajrangi_Bhaijaan_Poster.jpg",
+    MovieDetails: "Kabir Khan",
+    link: "https://en.wikipedia.org/wiki/Bajrangi_Bhaijaan",
+  },
+];
