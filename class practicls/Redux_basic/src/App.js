48afffb85f0cf8cce98b49d1e40593dc96bae662
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-04-07 20:51:29 +0530

    Add files via upload

---
+++ class practicls/Redux_basic/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Number from './Components/Number';
+
+function App() {
+  return (
+    <div className='container'>
+      <Number />
+    </div>
+  );
+}
+
+export default App;
