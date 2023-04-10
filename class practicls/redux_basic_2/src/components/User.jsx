32ccc6a0876e8736ed1c49e1e9c54eae0cc3940c
RAJ D. MORADIYA <110352291+RDMoradiya@users.noreply.github.com>
2023-04-10 23:52:59 +0530

    Add files via upload

--- class practicls/redux_basic_2/src/components/User.jsx
+++ class practicls/redux_basic_2/src/components/User.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ChangeHandler } from "../Redux/UserSlice";
 
 function User() {
   const { firstname, lastname } = useSelector((state) => state.UserSlice);
   const Dispatch = useDispatch();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   return (
-    <div className="container mt-5 p-5 border border-2 border-primary">
-      <h3 htmlFor="firstname" className="Form-label">
+    <div className="container mt-5 p-5 border border-2 border-danger rounded-5">
+      <label htmlFor="firstname" className="Form-label mb-2">
         First Name :- {firstname}
-      </h3>
+      </label>
+      <input
+        type="text"
+        className="form-control border border-primary"
+        placeholder="Enter Firstname"
+        onChange={(e) => setFirstName(e.target.value)}
+        value={firstName}
+      />
       <br />
-      <h3 htmlFor="lastname" className="Form-label">
+      <label htmlFor="lastname" className="Form-label mb-2">
         Last Name :- {lastname}
-      </h3>
+      </label>
+      <input
+        type="text"
+        className="form-control border border-primary"
+        placeholder="Enter Lastname"
+        onChange={(e) => setLastName(e.target.value)}
+        value={lastName}
+      />
       <button
-        className="btn btn-secondary p-2 mt-2"
-        onClick={() => Dispatch(ChangeHandler())}
+        className="btn btn-success px-3 mt-2"
+        onClick={() => Dispatch(ChangeHandler({ firstName, lastName }))}
       >
         Change
       </button>
